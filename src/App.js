d653e13b21e99cf6e169ba8798ae8655c8ef7a6a
denise <dmartini742@gmail.com>
2020-09-25 16:43:52 +0200

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import './tailwind.output.css'
+import axios from 'axios'
 function App() {
+
+const res = async () => {
+    const res = await axios.get("https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?=javascript&location=uk")
+    console.log(res)
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<button onClick={res}>Click</button>
     </div>
   );
 }
