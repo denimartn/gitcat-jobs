d5f0bdd22bba36fcea97db8c5ab5fae5c60fd645
denise <dmartini742@gmail.com>
2020-11-07 19:05:58 +0100

    add(navbar): add navbar to app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import './App.css';
-import './tailwind.output.css'
-import axios from 'axios'
+import React, { useState } from "react";
+import "./App.css";
+import "./NavBar.js"
+import "./tailwind.output.css";
+import axios from "axios";
+import NavBar from "./NavBar.js";
 function App() {
+  const [jobs, setJobs] = useState([]);
+  React.useEffect(() => {
+    async function fetchData() {
+      let res = await axios.get(
+        "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=javascript&location=milan"
+      );
+      console.log(res);
+      let newArr = [];
+      for (let job of res.data) {
+        newArr.push(job);
+      }
+      setJobs(newArr);
+    }
+    console.log(jobs);
+    fetchData();
+  });
 
-const res = async () => {
-    const res = await axios.get("https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?=javascript&location=uk")
-    console.log(res)
-    
-  }
   return (
-    <div className="App">
-<button onClick={res}>Click</button>
-    </div>
+ <NavBar/>
   );
 }
 
