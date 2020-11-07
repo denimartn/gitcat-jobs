a2a7c77bb507109388b6c7d0a33f862e43c75475
denise <dmartini742@gmail.com>
2020-11-07 19:04:41 +0100

    create(component): create navbar component

---
+++ src/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./tailwind.output.css";
+function NavBar() {
+    return (
+        
+        <nav className="bg-black">
+  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+  <div class="relative flex items-center  h-16">
+  <div class=" flex items-center justify-center sm:items-stretch sm:justify-start">
+      </div>
+      <img class="block lg:hidden h-8 w-auto" src="/assets/logo.png" alt="github logo"/>
+      </div>
+      </div>
+        </nav>
+       
+      )
+}
+
+export default NavBar;
