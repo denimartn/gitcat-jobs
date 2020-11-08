5db592b7942b298540c44bd08bb4833ec7ae49b4
denise <dmartini742@gmail.com>
2020-11-08 19:37:33 +0100

    change(navbar): logo image size

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,13 +3,17 @@ import "./tailwind.output.css";
 function NavBar() {
     return (
         
-        <nav className="bg-black">
+        <nav className=" px-32" style={{backgroundColor: "#24292e"}}>
   <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
   <div class="relative flex items-center  h-16">
   <div class=" flex items-center justify-center sm:items-stretch sm:justify-start">
       </div>
-      <img class="block lg:hidden h-8 w-auto" src="/assets/logo.png" alt="github logo"/>
+      <div className="w-8">
+      <img className="w-full" src="/assets/logo.png" alt="github logo"/>
       </div>
+
+      </div>
+      
       </div>
         </nav>
        
