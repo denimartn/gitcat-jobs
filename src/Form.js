399999a1f5c35d138e2b49275fdc30fc7185e9d9
denise <dmartini742@gmail.com>
2020-11-22 17:46:28 +0100

    style form and navbar

--- src/Form.js
+++ src/Form.js
@@ -20,11 +20,16 @@ function Form() {
                     Location
                   </label>
                   <input className="mt-1 px-1 py-1 border rounded focus:outline-none focus:shadow-outline focus:border-blue-400 " />
-                </div>  
-              </div>
+                </div>
 
-              <div className="flex items-end">
-                <div>
+                <div className="flex items-end ">
+                  <div className='mr-4 ml-4 flex items-center '>
+                  <span class="text-sm font-medium leading-5 text-gray-700 mr-2 cursor-pointer">Full time only</span>
+                  <input
+                    type="checkbox"
+                    className="form-tick  h-4 w-4 border border-gray-300 rounded-md checked:bg-indigo-600 checked:border-transparent focus:outline-none"
+                  />
+                  </div>
                   <button className="py-2 px-2  rounded-md font-medium leading-5 text-white bg-indigo-600 shadow-sm hover:bg-indigo-500 focus:outline-none focus:shadow-outline-blue active:bg-indigo-600   ">
                     Search
                   </button>
--- src/NavBar.js
+++ src/NavBar.js
@@ -4,9 +4,9 @@ function NavBar() {
     return (
         
         <nav className=" px-32" style={{backgroundColor: "#24292e"}}>
-  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-  <div class="relative flex items-center  h-16">
-  <div class=" flex items-center justify-center sm:items-stretch sm:justify-start">
+  <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+  <div className="relative flex items-center  h-16">
+  <div className=" flex items-center justify-center sm:items-stretch sm:justify-start">
       </div>
       <div className="w-8">
       <img className="w-full" src="/assets/logo.png" alt="github logo"/>
