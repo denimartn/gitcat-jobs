6ba52576f42ec34940f5e653a03b2bd82f33b847
denimartn <dmartini742@gmail.com>
2021-02-27 17:40:05 +0100

    style form responsive

--- src/Form.js
+++ src/Form.js
@@ -2,28 +2,24 @@ import React from "react";
 
 function Form() {
   return (
-    <div className="px-32">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 my-6 ">
-        <div>
-          <form>
-            <div className="flex justify-between">
-              <div className="flex flex-row">
-                <div className="mr-6">
-                  <label className="block text-sm font-medium leading-5 text-gray-700">
+          <form className="sm:px-40 my-16">
+              <div className="flex sm:flex-row flex-col w-full">
+                <div className="sm:mr-6 px-6 sm:px-0">
+                  <label className="text-sm font-medium text-gray-700">
                     Job description
                   </label>
-                  <input className="mt-1 px-1 py-1 border rounded focus:outline-none focus:shadow-outline focus:border-blue-400" />
+                  <input className="w-full mt-1 px-1 py-1 border rounded focus:outline-none focus:shadow-outline focus:border-blue-400" />
                 </div>
-                <div className="">
-                  <label className="block text-sm font-medium leading-5 text-gray-700">
+                <div className="sm:mr-6 px-6 sm:px-0">
+                  <label className="text-sm font-medium text-gray-700">
                     Location
                   </label>
-                  <input className="mt-1 px-1 py-1 border rounded focus:outline-none focus:shadow-outline focus:border-blue-400 " />
+                  <input className="w-full mt-1 px-1 py-1 border rounded focus:outline-none focus:shadow-outline focus:border-blue-400 " />
                 </div>
 
-                <div className="flex items-end ">
-                  <div className='mr-4 ml-4 flex items-center '>
-                  <span class="text-sm font-medium leading-5 text-gray-700 mr-2 cursor-pointer">Full time only</span>
+                <div className="flex sm:items-end flex-col sm:flex-row px-6 sm:px-0">
+                  <div className='sm:mr-4 sm:ml-4 mb-6 mt-6 sm:mt-0 sm:mb-0 flex items-center '>
+                  <span class="text-sm font-medium text-gray-700 mr-2 cursor-pointer">Full time only</span>
                   <input
                     type="checkbox"
                     className="form-tick  h-4 w-4 border border-gray-300 rounded-md checked:bg-indigo-600 checked:border-transparent focus:outline-none"
@@ -34,11 +30,7 @@ function Form() {
                   </button>
                 </div>
               </div>
-            </div>
           </form>
-        </div>
-      </div>
-    </div>
   );
 }
 
