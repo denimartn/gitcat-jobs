758daa33f2110362422d5b5fd3b23620d7f8e317
denimartn <dmartini742@gmail.com>
2021-03-07 17:00:56 +0100

    style job detail

--- src/JobDetail.js
+++ src/JobDetail.js
@@ -23,13 +23,28 @@ function JobDetail({ jobs }) {
   return (
     <div className="sm:px-60 px-6 my-16">
       <Link to="/">
-        <button class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-green-500 text-white font-bold px-4 rounded-md mb-8">
+        <button className="flex transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-green-500 text-white font-bold px-2 py-1 rounded-md mb-8">
+          <svg
+          className="w-6 mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <div>
+
           See all positions
+          </div>
         </button>
       </Link>
-      <div className="flex w-full flex-col bg-white p-4">
-        <div className="mb-8 flex">
-          <div>
+      <div className="flex w-full flex-col bg-white p-4 sm:p-8">
+        <div className="mb-8 flex sm:justify-between w-full">
+          <div className="w-full">
             <div className="text-2xl font-extrabold text-gray-900 tracking-tight mb-4">
               {job.title}
             </div>
@@ -37,8 +52,10 @@ function JobDetail({ jobs }) {
               {job.type}
             </div>
           </div>
-          <div className="text-right">
-            <div className="mb-2 text-md truncate w-full normal-case font-normal -mt-1 text-gray-500">{job.company}</div>
+          <div>
+            <div className="text-right mb-2 text-md truncate w-full normal-case font-normal -mt-1 text-gray-500">
+              {job.company}
+            </div>
             <img src={job.company_logo} alt="company logo" />
           </div>
           <hr />
@@ -47,7 +64,19 @@ function JobDetail({ jobs }) {
           <div className="text-2xl font-extrabold text-gray-900 tracking-tight mb-4">
             Description
           </div>
-          <div className="text-gray-500 text-lg" dangerouslySetInnerHTML={{ __html: job.description }}></div>
+          <div
+            className="text-gray-500 text-lg"
+            dangerouslySetInnerHTML={{ __html: job.description }}
+          ></div>
+        </div>
+        <div className="mb-8">
+          <div className="text-2xl font-extrabold text-gray-900 tracking-tight mb-4">
+            Apply now 👇
+          </div>
+          <div
+            className="text-gray-500 text-sm"
+            dangerouslySetInnerHTML={{ __html: job.how_to_apply }}
+          ></div>
         </div>
       </div>
     </div>
