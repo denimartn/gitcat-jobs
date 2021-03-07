df340f4e5b55a6e4c28767c360861d949a58504a
denimartn <dmartini742@gmail.com>
2021-03-07 16:30:31 +0100

    style jobdetail

--- src/JobDetail.js
+++ src/JobDetail.js
@@ -23,10 +23,33 @@ function JobDetail({ jobs }) {
   return (
     <div className="sm:px-60 px-6 my-16">
       <Link to="/">
-        <button class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-green-500 text-white font-bold px-4 rounded-md">
+        <button class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-green-500 text-white font-bold px-4 rounded-md mb-8">
           See all positions
         </button>
       </Link>
+      <div className="flex w-full flex-col bg-white p-4">
+        <div className="mb-8 flex">
+          <div>
+            <div className="text-2xl font-extrabold text-gray-900 tracking-tight mb-4">
+              {job.title}
+            </div>
+            <div className="text-sm truncate w-full -mt-1 text-green-500 font-bold mb-4">
+              {job.type}
+            </div>
+          </div>
+          <div className="text-right">
+            <div className="mb-2 text-md truncate w-full normal-case font-normal -mt-1 text-gray-500">{job.company}</div>
+            <img src={job.company_logo} alt="company logo" />
+          </div>
+          <hr />
+        </div>
+        <div className="mb-8">
+          <div className="text-2xl font-extrabold text-gray-900 tracking-tight mb-4">
+            Description
+          </div>
+          <div className="text-gray-500 text-lg" dangerouslySetInnerHTML={{ __html: job.description }}></div>
+        </div>
+      </div>
     </div>
   );
 }
