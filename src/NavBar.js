eb8989de5e91385610f6cc17e4efc38d47a98a40
denimartn <dmartini742@gmail.com>
2021-03-13 22:40:22 +0100

    change logo and style fixes

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,8 +5,9 @@ function NavBar() {
       className="w-full"
       style={{ backgroundColor: "#24292e" }}
     >
-      <div className="sm:px-60">
-        <img className="w-14 p-2" src="/assets/logo.png" alt="github logo" />
+      <div className="sm:px-60 flex items-center">
+        <img className="w-14 p-2 mr-4" src="/assets/illustration/logo.png" alt="github logo" />
+       <div className="text-xl sm:text-2xl text-purple-100 font-semibold">Amazing jobs listing</div> 
       </div>
     </div>
   );
